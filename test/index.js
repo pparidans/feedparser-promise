84487a573ef8b89f65facee30bde5ab96aea3ba2
Pierre Paridans <pierre@paridans.com>
2016-05-16 23:36:07 +0200

    use chai & chaiAsPromised to strenghten tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,21 @@
 const FeedParserPromise = require('../index')
 const fs = require('fs')
+const chai = require('chai')
+const expect = chai.expect
+const chaiAsPromised = require('chai-as-promised')
 
-const testFeed = __dirname + '/feed.xml'
+chai.use(chaiAsPromised)
 
 describe('FeedParserPromise', function() {
 
   it('should read a valid feed from filesystem and parse it', function() {
-    const fsFeed = fs.createReadStream(testFeed)
-    FeedParserPromise(fsFeed)
-      .then( function(feed) { assert(typeof(feed)) === 'Object' } )
-      .catch( function(err) { new Error('Feed parsing failed!', err) } )
+    const feedStream = fs.createReadStream(__dirname + '/feed.xml')
+    return expect(FeedParserPromise(feedStream)).to.eventually.be.an('array')
+  })
+
+  it('an invalid feed should throw a "catchable"  error', function() {
+    const brokenFeedStream = fs.createReadStream(__dirname + '/notafeed.xml')
+    return expect(FeedParserPromise(brokenFeedStream)).to.be.rejected
   })
 
 })
